f77ba196d129719e461b357fcf6197b60cb4b2a7
andrejgoncarenko <graf2100@gmail.com>
2018-05-15 22:43:19 +0300

    start

---
+++ index.js
@@ -0,0 +1,31 @@
+const WebSocket = require('ws');
+
+const server = new WebSocket.Server({port: 5000});
+
+const profiles = [{
+    id: 223,
+    code: 'T-23',
+    type: '2-пазовый',
+    series: 'Оптима',
+    height: '45',
+    width: '45',
+    length: '6',
+    price: '240',
+    colors: 'по RAL',
+    provider: 'Петралюм',
+    description: 'Открыть',
+    images: '/img/Optima1 (1).png',
+    scheme: '/img/Optima1 (1).png',
+}];
+
+server.on('connection', ws => {
+    ws.on('message', message => {
+        profiles.push(JSON.parse(message));
+        server.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify(profiles));
+            }
+        });
+    });
+    ws.send(JSON.stringify(profiles));
+});
