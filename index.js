fa1021b1deecacc4afbe3969a10e71374f03f468
andrejgoncarenko <graf2100@gmail.com>
2018-05-16 19:48:27 +0300

    test

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/*
 const express = require('express');
 const app = express();
 const WebSocket = require('ws');
@@ -43,14 +42,5 @@ app.ws('/', ws => {
     ws.send(JSON.stringify(profiles));
 });
 
-app.listen(8080);*/
-const express = require('express');
-const app = express();
+app.listen(8080);
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
-
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!');
-});
