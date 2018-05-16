1913ead27280cad1e08583d17378a197c17c6594
andrejgoncarenko <graf2100@gmail.com>
2018-05-16 14:36:38 +0300

    new ws

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+
+
 /*const WebSocket = require('ws');
 const expressWs = require('express-ws')(app);
 
